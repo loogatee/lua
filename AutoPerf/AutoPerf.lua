476bdfec6a466a0e3d51f29daefdca3701c5f288
John Reed <jreed@ice-energy.com>
2018-02-28 10:56:41 -0700

    Completed initial functionality. Cannot yet do:     - recovery due to watchdog     - transition into melt

--- AutoPerf/AutoPerf.lua
+++ AutoPerf/AutoPerf.lua
@@ -1,3 +1,11 @@
+
+
+DEFAULT_DIRECTORY = "C:\\PerfDataFiles"
+DEFAULT_IPADDR    = "192.168.11.115"
+
+
+
+
 require( "iuplua" )
 require( "iupluacontrols" )
 socket = require 'socket' 
@@ -7,8 +15,6 @@ require( "alien"  )
 require( "math"   )
 bit = require( "bit" )
 
-DEFAULT_DIRECTORY = "C:\\PerfDataFiles"
-DEFAULT_IPADDR    = "192.168.11.115"
 
 
 
@@ -20,6 +26,7 @@ DIALOG_WIDTH = "                                                            "
 
 SYSLOG_PATH = DEFAULT_DIRECTORY .. "\\APerfSysLog.txt"
 
+logfileFD = ''
 
 --
 -- How to avoid popup a window when use os.execute in lua:
@@ -37,6 +44,10 @@ WshShell.Run "%comspec% /c C:\windows\system32\notepad.exe ]] .. SYSLOG_PATH .. 
 Set WshShell = Nothing
 ]]
 
+LOOK_NONE               = 0
+LOOK_START_MAKE         = 1
+LOOK_EEV_CONTROL_ACTIVE = 2
+LOOK_MAKE_COMPLETE      = 3
 
 COLOR_YELLOW   = 0
 COLOR_GRAY     = 1
@@ -51,6 +62,8 @@ RETV_SUCCEED   = 2
 MSTATE_CMD_NONE           = 0
 MSTATE_CMD_TEST_CONN1     = 1
 MSTATE_CMD_GET_SERIALNUM  = 2
+MSTATE_MONITOR_AND_LOG    = 3
+MSTATE_DO_TERMINATE       = 4
 
 
 OUTFILES_PATH = DEFAULT_DIRECTORY .. "\\tmp"
@@ -64,12 +77,18 @@ begin_finish     = PROGRAM_BEGIN
 color_toggle     = COLOR_GRAY
 completion_time  = 0
 
+times_no_data = 0
+
 SUCCESS_MSG = " *** Success! *** "
 FAIL_MSG    = "   *** FAIL ***   "
 BLANK_MSG   = "                  "
 
 First_Comms_Has_Run = false
 First_Comms_Addr    = ""
+GLOBAL_fname        = ""
+GLOBAL_dt           = 0
+GLOBAL_Status       = "None                               "
+look_for_state      =  LOOK_NONE
 
 timer1 = iup.timer{ time=1000,  run="NO" }               -- 1000 is 1 second
 
@@ -224,8 +243,7 @@ function FirstComms()
 
     SS = table.concat(T)
 
-    if #T > 1 and string.find(SS,'COOLDATA') ~= nil and string.find(SS,'Lua.org,') ~= nil and 
-                     T[#T-1] == '>' and T[#T] == ' ' then
+    if #T > 1 and SS:find('COOLDATA') ~= nil and SS:find('Lua.org,') ~= nil and T[#T-1] == '>' and T[#T] == ' ' then
         SysPrint(SS)
         First_Comms_Has_Run = true
         return 1
@@ -291,6 +309,113 @@ function CMD_Test_Conn1()
     main_state = MSTATE_CMD_NONE
 end
 
+function ltrim(s)
+  return (s:gsub("^%s*", ""))
+end
+
+
+function CMD_Monitor_Log()
+    local Status,T,SS
+
+    do_color_toggle(img_4)
+
+    Status = ''
+    T      = {}
+
+    while Status ~= 'timeout' do
+        SS,Status = conn:receive(1)
+        if SS ~= nil then T[#T+1] = SS end
+    end
+
+    if #T > 1 then
+
+        SS = table.concat(T)
+
+        if look_for_state == LOOK_START_MAKE then
+            if SS:find("Start Make") ~= nil then
+                look_for_state = LOOK_EEV_CONTROL_ACTIVE
+                lbl_SM.title   =  "Make, Looking for 'EEV control active'"
+            end
+        elseif look_for_state == LOOK_EEV_CONTROL_ACTIVE then
+            if SS:find("EEV control active") ~= nil then
+                look_for_state = LOOK_MAKE_COMPLETE
+                lbl_SM.title   =  "Make, Looking for 'Make complete'"
+            end
+        elseif look_for_state == LOOK_MAKE_COMPLETE then
+
+            if SS:find("Make complete") ~= nil then
+                look_for_state = LOOK_NONE
+                lbl_SM.title   = "Make complete"
+                main_state = MSTATE_DO_TERMINATE                         -- state machine the terminate function
+                conn:send("setcvar('SingleMake','0')\n")                 -- Terminates the 1-second data
+                terminate_count = 0                                      -- state machine uses this to count
+            else
+                local n1,n2,S1,S2
+
+                S1="x";S2="x"
+
+                n1 = SS:find("SYp="); if n1 ~= nil then
+                    n2 = SS:find("CSp",n1)
+                    if n2 ~= nil then S1 = SS:sub(n1+4,n2-2) end
+                end
+
+                n1 = SS:find("TWt="); if n1 ~= nil then
+                    n2 = SS:find("CBt",n1)
+                    if n2 ~= nil then S2 = SS:sub(n1+4,n2-2) end
+                end
+
+                lbl_SM.title = string.format("Make, SYp=%s TWt=%s",ltrim(S1),ltrim(S2))
+            end
+
+        end
+
+        logfileFD:write(SS)
+        logfileFD:flush()
+        io.write(SS)
+        io.flush()
+        times_no_data = 0
+
+    else
+        times_no_data = times_no_data + 1
+        if times_no_data > 100 then
+            times_no_data = 0
+            io.write( "zzz 100 Times zzz\n" )
+            io.flush()
+        end
+    end
+
+    timer1.run = "YES"
+
+end
+
+
+--
+--
+--
+function CMD_Do_Terminate()
+
+    terminate_count = terminate_count + 1
+
+    if terminate_count == 1 then
+        conn:send("setcvar('SingleMelt','0')\n");           os.sleep(180,1000);  rddump()
+    elseif terminate_count == 2 then
+        conn:send("setcvar('AlwaysDisallowMelt','1')\n");   os.sleep(180,1000);  rddump()
+    elseif terminate_count == 3 then
+        conn:send("setcvar('AlwaysDisallowMake','1')\n");   os.sleep(180,1000);  rddump()
+    end
+
+    if terminate_count <= 5 then
+        CMD_Monitor_Log()
+    else
+        main_state   = MSTATE_CMD_NONE
+        img_4.image  = img_gray                                  --     Change color to gray
+        color_toggle = COLOR_GRAY                               --     update indicator
+        logfileFD:close()
+        SysPrint( "xxxxxxxxxx Terminate\n" )
+        conn:send("setcvar('SysProductionMode','0')\n");  os.sleep(180,1000); rdshow()
+    end
+end
+
 
 --
 --
@@ -300,6 +425,8 @@ function timer1:action_cb()
 
     if     main_state == MSTATE_CMD_TEST_CONN1     then   CMD_Test_Conn1()
     elseif main_state == MSTATE_CMD_GET_SERIALNUM  then   CMD_Get_SerialNum()
+    elseif main_state == MSTATE_MONITOR_AND_LOG    then   CMD_Monitor_Log()
+    elseif main_state == MSTATE_DO_TERMINATE       then   CMD_Do_Terminate()
     end
 
     return iup.DEFAULT
@@ -313,7 +440,10 @@ end
 function Close_Then_Connect()
     local R,S
 
-    conn:close()
+    if First_Comms_Addr ~= "" then
+        conn:close()
+    end
+
     First_Comms_Has_Run = false
     os.sleep(200,1000)
     conn = socket.tcp() 
@@ -331,28 +461,109 @@ function Close_Then_Connect()
     end
 end
 
+function rdshow()
+    local Status,T,SS
+
+    Status = ''; T = {}
+    while Status ~= 'timeout' do
+        SS,Status = conn:receive(1)
+        if SS ~= nil then T[#T+1] = SS end
+    end
+
+    SS = table.concat(T)
+    SysPrint(SS)
+end
+
+function rddump()
+    local Status,SS
+    Status = ''
+    while Status ~= 'timeout' do
+        SS,Status = conn:receive(1)
+    end
+end
 
 function btn_cb_MakeMelt(self)
+  local T,s1,fname
 
-    local T = os.date("*t",os.time())
-    print(string.format("M%s_%d%02d%02d_%02d%02d%02d",SN_tbox.value,T.year,T.month,T.day,T.hour,T.min,T.sec))
-    io.flush()
+  SysPrint(os.date() .. ':  btn MakeMelt\n')
+
+  if main_state == MSTATE_CMD_NONE then                                            -- NOP unless idle
+
+    if First_Comms_Has_Run == false or First_Comms_Addr ~= IP_tbox.value then      -- Assure Connectivity to the board
+        if do_CMD_Test_Conn1() == 0 then
+            iup.Message("Socket Error", "ERROR!\n\rconn:connect()")
+            return iup.DEFAULT
+        end
+    end
+
+    s1 = SN_tbox.value                                                             -- Retrieve/Validate the SerialNumber
+    if s1:len() ~= 5 or s1:find('%d%d%d%d%d') == nil then
+        iup.Message("BAD S/N", "ERROR!\n\rS/N is 5 Digits")
+        return iup.DEFAULT
+    end
+
+    T = os.date("*t",os.time())                                                    -- Assign Filename based on SN
+    fname = string.format("%s\\M%s_%d%02d%02d_%02d%02d%02d.txt",DEFAULT_DIRECTORY,SN_tbox.value,T.year,T.month,T.day,T.hour,T.min,T.sec)
+    GLOBAL_fname = fname
+
+    logfileFD = io.open(fname, 'w');                                               -- Open up the LogFile
+    if logfileFD == nil then
+        iup.Message("file open error", "ERROR opening:\n\r" .. fname)
+        return iup.DEFAULT
+    end
+
+    conn:send("setcvar('AlwaysDisallowMelt','1')\n");  os.sleep(150,1000); rddump()
+    conn:send("setcvar('AlwaysDisallowMake','0')\n");  os.sleep(150,1000); rddump()
+    conn:send("setcvar('SingleMelt',        '0')\n");  os.sleep(150,1000); rddump()
+    conn:send("setcvar('SysProductionMode', '3')\n");  os.sleep(150,1000); rddump()
+    conn:send("setcvar('SingleMake',        '1')\n");  os.sleep(150,1000); rddump()
+
+    main_state    = MSTATE_MONITOR_AND_LOG                                         -- We're logging and monitoring now
+    timer1.time   = 300                                                            -- timer at 300 milliseconds
+    timer1.run    = "YES"                                                          --    and GO!
+    times_no_data = 0                                                              -- for purposes of recovery
+
+    lbl_SM.title   =  "Make, Looking for 'Start Make'"
+    look_for_state =  LOOK_START_MAKE
+
+    Show_LogFile_Button()                                                          -- Hello!
+  end
+
+  return iup.DEFAULT
+end
+
+function btn_cb_Terminate(self)
+    SysPrint(os.date() .. ':  btn Terminate\n')
+
+    if main_state == MSTATE_MONITOR_AND_LOG then                 -- only terminate when in this state
+        main_state = MSTATE_DO_TERMINATE                         -- state machine the terminate function
+        conn:send("setcvar('SingleMake','0')\n")                 -- Terminates the 1-second data
+        terminate_count = 0                                      -- state machine uses this to count
+    end
 
     return iup.DEFAULT
 end
 
 
-
 function btn_cb_TestConnectivity(self)
+  SysPrint(os.date() .. ':  btn TestConnectivity\n')
+
+  if main_state == MSTATE_CMD_NONE then
     main_state   = MSTATE_CMD_TEST_CONN1
     img_C.image  = img_gray
     timer1.time  = 100
     timer1.run   = "YES"
-    return iup.DEFAULT
+  end
+
+  return iup.DEFAULT
 end
 
 
 function btn_cb_GetSN(self)
+  SysPrint(os.date() .. ':  btn GetSN\n')
+
+  if main_state == MSTATE_CMD_NONE then
+
     if First_Comms_Has_Run == false or First_Comms_Addr ~= IP_tbox.value then
         if do_CMD_Test_Conn1() == 0 then
             SN_tbox.value = 'xxxxx'
@@ -360,18 +571,23 @@ function btn_cb_GetSN(self)
         end
     end
 
-    SysPrint("Retrieving Serial Number\n")
     conn:send("Q=PB1_ConfigData.Board_Mac_Address; print(string.format('ZZ%05d',(Q[5]*256)+Q[6]))\n")
 
     main_state  = MSTATE_CMD_GET_SERIALNUM
-    timer1.time = 500
+    timer1.time = 500                                           
     timer1.run  = "YES"
-    return iup.DEFAULT
+
+    btn_ShowLog.visible = "NO"                                  -- Makes the 'ShowLog' button visible.
+  end
+
+  return iup.DEFAULT
 end
 
 
 
 function do_color_toggle(StatusImage)
+  if GLOBAL_dt == 1 then
+    GLOBAL_dt = 0
     if color_toggle == COLOR_GRAY then                          -- IF current color is Gray
         StatusImage.image = img_yellow                          --     Change color to yellow
         color_toggle = COLOR_YELLOW                             --     update indicator
@@ -379,17 +595,24 @@ function do_color_toggle(StatusImage)
         StatusImage.image = img_gray                            --     Change color to gray
         color_toggle = COLOR_GRAY                               --     update indicator
     end
+  else
+    GLOBAL_dt = 1
+  end
 end
 
 
 function Show_LogFile_Button()
-    local S = table.concat(FULL_CMD_LOG,'\n')                -- Command Log is kept in this variable: convert to a single string
 
-    os.remove(LOGF_PATH)                                     -- removes file 'logfile.txt', if found
-    local fd=io.open(LOGF_PATH, 'w')                         -- creates LogFile
-    fd:write(S);                                             -- writes data to the LogFile
-    fd:close()                                               -- Closes LogFile
+local tmps = [[
+Set WshShell = CreateObject("WScript.Shell")
+Set objFSO   = CreateObject("Scripting.FileSystemObject")
+WshShell.Run "%comspec% /c C:\windows\system32\notepad.exe ]] .. GLOBAL_fname .. [[", 0, true
+Set WshShell = Nothing
+]]
+    os.remove(C9_PATH)
+    fd=io.open(C9_PATH, 'w'); fd:write(tmps); fd:close()
 
+    btn_ShowLog.title   = " Show " .. GLOBAL_fname .. " "
     btn_ShowLog.visible = "YES"                              -- Makes the 'ShowLog' button visible.
 end
 
@@ -431,7 +654,8 @@ lbl_1    = iup.label { title = " IP Addr:    ",                      ALIGNMENT="
 lbl_SN   = iup.label { title = " S/N:        ",                      ALIGNMENT="ALEFT", font = "COURIER_NORMAL_14" }
 lbl_2    = iup.label { title = "                    ",               ALIGNMENT="ALEFT", font = "COURIER_NORMAL_14" }
 lbl_4    = iup.label { title = "Performing Scan:    ",               ALIGNMENT="ALEFT", font = "COURIER_NORMAL_14" }
-lbl_PF   = iup.label { title =  BLANK_MSG,                           ALIGNMENT="ALEFT", font = "COURIER_NORMAL_14" }
+lbl_SM   = iup.label { title =  GLOBAL_Status,                       ALIGNMENT="ALEFT", font = "COURIER_NORMAL_14" }
+lbl_ST   = iup.label { title = " Status:  ",                         ALIGNMENT="ALEFT", font = "COURIER_NORMAL_14" }
 
 img_4 = iup.label { image = img_gray, ALIGNMENT="ARIGHT:ABOTTOM" }
 img_C = iup.label { image = img_gray, ALIGNMENT="ARIGHT:ABOTTOM" }
@@ -445,7 +669,7 @@ btn_TestConny = iup.button {
 }
 
 btn_ShowLog = iup.button {
-    title         = " Show Logfile ",
+    title         = "               Show Logfile                        ",
     action        = Log_Button_CB,
     font          = "COURIER_NORMAL_11",
     impressborder = "YES",
@@ -497,6 +721,8 @@ lbl_emptI  = iup.label { title = "           ",  ALIGNMENT="ARIGHT:ATOP" }
 lbl_emptJ  = iup.label { title = " ",            ALIGNMENT="ARIGHT:ATOP",font="COURIER_NORMAL_14" }
 lbl_emptK  = iup.label { title = " ",            ALIGNMENT="ARIGHT:ATOP",font="COURIER_NORMAL_14" }
 lbl_emptL  = iup.label { title = "    ",         ALIGNMENT="ARIGHT:ATOP",font="COURIER_NORMAL_14" }
+lbl_emptN  = iup.label { title = "    ",         ALIGNMENT="ARIGHT:ATOP",font="COURIER_NORMAL_14" }
+lbl_emptO  = iup.label { title = "    ",         ALIGNMENT="ARIGHT:ATOP",font="COURIER_NORMAL_14" }
 lbl_emptF  = iup.label { title = DIALOG_WIDTH,   ALIGNMENT="ARIGHT:ATOP", font="COURIER_NORMAL_14" }
 lbl_emptH  = iup.label { title = DIALOG_WIDTH,   ALIGNMENT="ARIGHT:ATOP", font="COURIER_NORMAL_14" }
 lbl_vers   = iup.label { title = DIALOG_WIDTH,   ALIGNMENT="ARIGHT:ATOP", font="COURIER_NORMAL_14" }
@@ -549,10 +775,12 @@ dlg = iup.dialog {
                     iup.hbox{lbl_empt4},
                     iup.hbox{lbl_empt05,btn_MakeMelt,lbl_empt06,img_4,lbl_emptM,btn_Terminate},
                     iup.hbox{lbl_empt5},
-                    iup.hbox{lbl_PF},
+                    iup.hbox{lbl_emptO},
+                    iup.hbox{lbl_ST,lbl_SM},
                     iup.hbox{lbl_empt6},
                     iup.hbox{lbl_empt7},
                     iup.hbox{lbl_empt8,btn_ShowLog},
+                    iup.hbox{lbl_emptN},
                 },
             },
         iup.hbox{lbl_empt03},
